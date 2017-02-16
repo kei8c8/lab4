51f880a2d4fbedc8ce16c8e1756fa7732da4f410
Katie Innis <katieinnis@ATM0560401DYN.dhcp.missouri.edu>
2017-02-16 10:45:56 -0600

    lab4 version 1

---
+++ lab4.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : lab4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
