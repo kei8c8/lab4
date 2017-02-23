495dc8bd8798bb88b4cffbb6825895a2ba654f6b
Katie Innis <katieinnis@Katies-MacBook-Pro-3.local>
2017-02-22 23:53:40 -0600

    Version 5

--- lab4.c
+++ lab4.c
@@ -13,210 +13,237 @@
 #include <ctype.h>
 #include <string.h>
 
+//initialize functions
 void usage(void);
-double* openfile(int);
-double* offset(double, double*);
+int* openfile(int);
+double* offset(double, int*);
 void newFile(char*, double*, double);
-void copyFile(char*, double*);
-double* scale(double sca, double* nums);
-double average(double*);
-double act_max(double*);
+void copyFile(char*, int*);
+double* scale(double sca, int* nums);
+double average(int*);
+double act_max(int*);
+//found in one function, but can't return more
+//than one variable from that function
+//so global to use in multiple functions
 int length=0;
 int max;
 
 
 int main(int argc, char *argv[]) {
+	//initialize variables
 	int x=1;
-	double* inpNum=0;
+	int* inpNum=0;
 	int y=1;
 	int flag=0;
 	int num=0;
+	int flag1=0;
 
-
+	//scans through entire command line looking for -n
 	while(y < argc){
 		if((argv[y][0] == '-') && (argv[y][1] == 'n')){
-			printf("\n-n was found\n");
 			num = 0;
-			num = atoi(argv[y+1]);
 
-			if(num<1 || num>11){
+			if((y+1) >= argc){ // if no number is entered after -n
+				printf("\nError. Not enough information.\n"); //print error
+				usage(); //shows usage
+			}
+
+			num = atoi(argv[y+1]); // converts character to integer
+
+			if(num<1 || num>11){ //checks if option is out of range
 				usage();
 			}
-			else{
-				inpNum = openfile(num);
-				flag = 1;
+			else{ //if in range
+				inpNum = openfile(num); //open file specified and get numbers
+				flag = 1; //file is found and opened
 			}
 
-			y++;
+			y++; //increments past y+1 because it's already found and used
 		}
 		y++;
 	}
 
-	if(flag != 1){
-		usage();
+	if(flag != 1){ //if file is never found and open
+		printf("\nError. Not enough information.\n");
+		usage(); //
 	}
 
-	while(x < argc){
+	while(x < argc){ //looks for other command line arguments
 
 		if((argv[x][0] == '-') && (argv[x][1] == 'n')){
-			x++;
+			flag1 = 1;
+			x++; //if found, skip because file has already been opened before
 		}
 		else if((argv[x][0] == '-') && (argv[x][1] == 'o')){
-			printf("\n-o was found\n");
+			flag1 = 1;
+			//initializes variables
 			int num1;
 			double* new_array;
-			char* fn;
+			char* fn=0;
 			double off=0;
-			num1 = sscanf(argv[x+1], "%lf", &off);
 
-			if(num1==0){
+			if((x+1) >= argc){ //checks if there is an argument entered after the -o
+				printf("\nError. Not enough information.\n");
+				usage();
+			}
+
+			num1 = sscanf(argv[x+1], "%lf", &off); //checks if offset contains characters
+
+			if(num1==0){  //if yes, print error
 				printf("\nError.\n");
 				usage();
 			}
 
-			new_array = offset(off, inpNum);
+			new_array = offset(off, inpNum); //calls offset and returns the new array
 
 			fn = calloc(19, sizeof(char)); //length of filename including null terminator
 
 			sprintf(fn, "Offset_data_%02d.txt", num);//prints to file name char*
 
-			newFile(fn, new_array, off);
+			newFile(fn, new_array, off); //puts new array in a new file
 
+			//frees allocated memory
 			free(new_array);
 			free(fn);
 			x++;
 		}
 		else if((argv[x][0] == '-') && (argv[x][1] == 's')){
-			printf("\n-s was found\n");
+			flag1 = 1;
+			//initializes variables
 			int num2;
 			double sca=0;
 			double* array2;
-			char* fn2;
-			num2 = sscanf(argv[x+1], "%lf", &sca);
+			char* fn2=0;
 
-			if(num2==0){
+			if((x+1) >= argc){ //prints usage if there is no argument after -s
+				printf("\nError. Not enough information.\n");
+				usage();
+			}
+
+			num2 = sscanf(argv[x+1], "%lf", &sca); //checks if number contains characters
+
+			if(num2==0){ //if yes, print error
 				printf("\nError.\n");
 				usage();
 			}
 
-			array2 = scale(sca, inpNum);
+			array2 = scale(sca, inpNum); //returns new, scaled array
 
 			fn2 = calloc(19, sizeof(char)); //length of filename including null terminator
 
 			sprintf(fn2, "Scaled_data_%02d.txt", num);//prints to file name char*
 
-			newFile(fn2, array2, sca);
+			newFile(fn2, array2, sca); //puts new array in a new file
 
+			//frees allocated memory
 			free(array2);
 			free(fn2);
 			x++;
 		}
 		else if((argv[x][0] == '-') && (argv[x][1] == 'r')){
-			printf("\n-r was found\n");
+			flag1 = 1;
+			//initializes variables
 			int len=0;
+			char* new_filename;
 
-			len = strlen(argv[x+1]);
+			new_filename = 0;
 
-			if(argv[x+1][len-1] != 't'){
-				printf("\nError.\n");
-				usage();
-			}
-			if(argv[x+1][len-2] != 'x'){
-				printf("\nError.\n");
-				usage();
-			}
-			if(argv[x+1][len-3] != 't'){
-				printf("\nError.\n");
-				usage();
-			}
-			if(argv[x+1][len-4] != '.'){
-				printf("\nError.\n");
+			if((x+1) >= argc){ //makes sure there is a string after -r
+				printf("\nError. Not enough information.\n");
 				usage();
 			}
 
-			copyFile( argv[x+1], inpNum);
+			len = strlen(argv[x+1]); //finds the length of the given string
+			//allocates enough memory for the string and the .txt added to it
+			new_filename = calloc((len+5), sizeof(char));
 
+			sprintf(new_filename, "%s.txt", argv[x+1]); //prints the new filename to a string
+			copyFile( new_filename, inpNum); //copies and renames the file
 
+			//frees allocated memory
+			free(new_filename);
 			x++;
 		}
 		else if((argv[x][0] == '-') && (argv[x][1] == 'h')){
-			printf("\n-h was found\n");
-			usage();
-
-			x++;
+			usage(); //prints usage
 		}
 		else if((argv[x][0] == '-') && (argv[x][1] == 'S')){
-			printf("\n-S was found\n");
+			flag1 = 1;
+			//initializes variables
 			double stats_max=0;
 			double stats_avg=0;
-			char* fn3;
+			char* fn3=0;
 
-			stats_max = act_max(inpNum);
-			stats_avg = average(inpNum);
+			stats_max = act_max(inpNum); //finds the maximum
+			stats_avg = average(inpNum); //determines the average of the array
 
-			fn3 = calloc(25, sizeof(char));
+			fn3 = calloc(25, sizeof(char)); //allocates space for file name
 
-			sprintf(fn3, "Statistics_data_%02d.txt", num);
+			sprintf(fn3, "Statistics_data_%02d.txt", num); //prints filename to string
 
 			FILE* f;
 
-			f = fopen(fn3, "w+");
+			f = fopen(fn3, "w+"); //creates a new file
 
 			fprintf(f, "%.4lf %.0lf", stats_avg, stats_max);
 
+			//frees allocated memory
 			fclose(f);
 			free(fn3);
-			x++;
 		}
 		else if((argv[x][0] == '-') && (argv[x][1] == 'C')){
-			printf("\n-C was found\n");
+			flag1 = 1;
+			//initializes variables
 			double avg=0;
 			double * new_centered;
 			char* fn4=0;
 
-			new_centered = calloc(length, sizeof(double));
-			fn4 = calloc(25, sizeof(char));
-			sprintf(fn4, "Centered_data_%02d.txt", num);
+			fn4 = calloc(25, sizeof(char)); //allocates memory for new filename
+			sprintf(fn4, "Centered_data_%02d.txt", num); //prints new filename to string
 
-			avg = average(inpNum);
-			avg *= (-1);
-			new_centered = offset(avg, inpNum);
+			avg = average(inpNum); //finds the average
+			avg *= (-1); //so it can be subtracted in the offset
+			new_centered = offset(avg, inpNum); //creates new, centered array
 
-			newFile(fn4, new_centered, avg);
+			newFile(fn4, new_centered, avg); //put new array into a new file
 
+			//frees allocated memory
+			free(fn4);
 			free(new_centered);
-			x++;
 		}
 		else if((argv[x][0] == '-') && (argv[x][1] == 'N')){
-			printf("\n-N was found\n");
+			flag1 = 1;
+			//initializes variables
 			double new_max;
 			double * new_normalized;
 			char* fn5=0;
 
-			new_normalized = calloc(length, sizeof(double));
-			fn5 = calloc(25, sizeof(char));
+			fn5 = calloc(25, sizeof(char)); //allocates memory for filename
 			sprintf(fn5, "Normalized_data_%02d.txt", num);
 
-			new_max = ((double) 1 / (double) max);
-			new_normalized = scale(new_max, inpNum);
+			new_max = ((double) 1 / (double) max); //scale it by dividing by the max
+			new_normalized = scale(new_max, inpNum); //creates new array
 
-			newFile(fn5, new_normalized, new_max);
+			newFile(fn5, new_normalized, new_max); //puts new array into a new file
 
+			//frees allocated memory
+			free(fn5);
 			free(new_normalized);
-			x++;
 		}
-		else{
-			usage();
+
+		if(flag1 != 1){
+			usage(); //if doesn't match any, print usage
 		}
 
 		x++;
 	}
 
+	//frees allocated memory
 	free(inpNum);
 	return 0;
 }
 
-void usage(void){
+void usage(void){ //prints usage
 	printf("\nUsage:");
 	printf("\n\t-n <File number between 1 and 11>\tSelects the file to open.");
 	printf("\n\t-o <Offset value>                \tOffsets the file numbers by the given value.");
@@ -225,17 +252,18 @@ void usage(void){
 	printf("\n\t-h                               \tHelp. Displays usage.");
 	printf("\n\t-S                               \tShows statistics.");
 	printf("\n\t-C                               \tCenters the signal.");
-	printf("\n\t-N                               \tNormalizes the signal.\n\n");
-	exit(0);
+	printf("\n\t-N                               \tNormalizes the signal.\n");
+	printf("\nExamples:\n   ./lab4 -n 2 - s 6.7\n   ./lab4 -C -n 4\n\n");
+	exit(0); //quits program
 }
 
-double* openfile(int num){
+int* openfile(int num){
 
-	FILE* file=0;
-	double* n=0;
+	FILE* file=0; //creates pointer to file
+	int* n=0;
 	int i=0;
 
-	switch(num){
+	switch(num){ //choses file based on number entered on the command line
 	case 1:
 		file = fopen("Raw_data_01.txt", "r");
 		break;
@@ -270,37 +298,35 @@ double* openfile(int num){
 		file = fopen("Raw_data_11.txt", "r");
 		break;
 	default:
-		printf("\nNot a valid option.\n");
-		usage();
+		printf("\nNot a valid option.\n"); //error if out of bounds
+		usage(); //prints usage
 		break;
 	}
 
 	if(file == NULL){
 		printf("\nError opening file.\n");
-		exit(0);
+		exit(0); //exit if file couldn't be opened
 	}
 
-	fscanf(file, "%d %d", &length, &max);
-	//test
-	printf("\n%lf", (double)max);
+	fscanf(file, "%d %d", &length, &max); //read in length and max from the file
 
-	n = calloc(length, sizeof(double));
+	n = calloc(length, sizeof(int)); //allocate memory for the array
 
 	for(i=0; i<length; i++){
-		fscanf(file, "%lf", &n[i]);
+		fscanf(file, "%d", &n[i]); //read in the numbers from the file
 	}
 
-	fclose(file);
+	fclose(file); //closes the file
 	return n;
 }
 
-double* offset(double off, double* nums){
+double* offset(double off, int* nums){
 	int x=0;
 	double* new;
-	new = calloc(length, sizeof(double));
+	new = calloc(length, sizeof(double)); //allocate memory for the new array
 
 	for(x=0;x<length;x++){
-		new[x] = (double) (nums[x] + off);
+		new[x] = (double) (nums[x] + off); //adds offset to array of numbers
 	}
 
 	return new;
@@ -310,73 +336,71 @@ void newFile(char* filename, double* numbers1, double off_sca){
 	FILE* fp=0;
 	int x=0;
 
-	fp = fopen(filename, "w+");
+	fp = fopen(filename, "w+"); //creates new file
 
-	fprintf(fp, "%d %.4lf", length, off_sca);
+	fprintf(fp, "%d %.4lf", length, off_sca); //prints the length and offset/scale
 
 	for(x=0;x<length;x++){
-		fprintf(fp, "\n%.4lf", numbers1[x]);
+		fprintf(fp, "\n%.4lf", numbers1[x]); //prints all the new numbers from the arrays
 	}
 
-	fclose(fp);
+	fclose(fp); //closes file
 	return;
 }
 
-double* scale(double sca, double* nums){
+double* scale(double sca, int* nums){
 	int x=0;
 	double* new;
-	new = calloc(length, sizeof(double));
+	new = calloc(length, sizeof(double)); //allocates memory for the array
 
 	for(x=0;x<length;x++){
-		new[x] = (double) (nums[x] * sca);
+		new[x] = (double) (nums[x] * sca); //creates the array of scaled numbers
 	}
 
 	return new;
 }
 
-void copyFile(char* fn, double* nums){
+void copyFile(char* fn, int* nums){
 	FILE* fp=0;
 	int x=0;
 
-	fp = fopen(fn, "w+");
+	fp = fopen(fn, "w+"); //opens a file to write to
 
-	fprintf(fp, "%d %d", length, max);
-	//test
-	printf("\n%lf", (double)max);
+	fprintf(fp, "%d %d", length, max); //prints the length and max to the file
 
 	for(x=0;x<length;x++){
-		fprintf(fp, "\n%.0d", (int) nums[x]);
+		fprintf(fp, "\n%d", (int) nums[x]); //prints numbers to the new file
 	}
 
-	fclose(fp);
+	fclose(fp); //closes file
 	return;
 }
 
-double act_max(double* orig_nums){
+double act_max(int* orig_nums){
 	int x=0;
 	double actual_max=0;
 
-	actual_max = orig_nums[0];
+	actual_max = (double) orig_nums[0]; //makes initial max the first number in the array
 
 	for(x=0;x<length;x++){
-		if(orig_nums[x]>actual_max){
-			actual_max = orig_nums[x];
+		if((double) orig_nums[x]>actual_max){ //compares each number to the max
+			actual_max = (double) orig_nums[x]; //if larger, then make it the max
 		}
 	}
 
 	return actual_max;
 }
 
-double average(double* orig_nums){
+double average(int* orig_nums){
 	int i=0;
 	double sum=0;
 	double average=0;
 
 	for(i=0;i<length;i++){
-		sum += orig_nums[i];
+		sum += (double) orig_nums[i]; //adds all the numbers in the array together
 	}
 
-	average = (double) sum / length;
+	average = (double) sum / length; //divides sum by the amount of numbers in the array
 
 	return average;
 }
